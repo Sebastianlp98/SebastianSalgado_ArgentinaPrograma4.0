f386501999ea13abc8b81940829217e6242fcc68
Sebastianlp98 <126526039+Sebastianlp98@users.noreply.github.com>
2023-04-10 19:24:59 -0300

    Create ExcepcionCarritoPrecioNulo.java

---
+++ Clase08/ejercicio2/ExcepcionCarritoPrecioNulo.java
@@ -0,0 +1,6 @@
+package guia08.ej02;
+
+public class ExcepcionCarritoPrecioNulo extends RuntimeException {
+	//solamente indicamos que se trata de una "RuntimeExcepcion" gracias a la herencia
+	//será de este tipo ya que es una excepción que se puede dar en tiempo de ejecución (puede ser evitada)
+}
