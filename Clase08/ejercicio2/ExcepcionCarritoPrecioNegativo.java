a88af53c34841d312a3a5c16cc3831bffdeed18d
Sebastianlp98 <126526039+Sebastianlp98@users.noreply.github.com>
2023-04-10 19:24:33 -0300

    Create ExcepcionCarritoPrecioNegativo.java

---
+++ Clase08/ejercicio2/ExcepcionCarritoPrecioNegativo.java
@@ -0,0 +1,7 @@
+package guia08.ej02;
+
+public class ExcepcionCarritoPrecioNegativo extends RuntimeException {
+	//hacemos lo mismo que en la excepción anterior!
+	//la diferencia radica en que son dos clases de excepciones que se usan en diferentes situaciones
+	//por este motivo, creamos dos clases de excepciones distintas
+}
