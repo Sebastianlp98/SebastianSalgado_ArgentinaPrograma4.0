e5da195f04855414c0c70d93c20a2d1b7b472579
Sebastianlp98 <126526039+Sebastianlp98@users.noreply.github.com>
2023-04-10 19:25:24 -0300

    Create ItemCarrito.java

---
+++ Clase08/ejercicio2/ItemCarrito.java
@@ -0,0 +1,25 @@
+package guia08.ej02;
+
+public class ItemCarrito {
+	
+	private Producto producto;
+	private int cantidadProducto;
+	
+	public ItemCarrito(Producto producto) {
+		this.producto=producto; 
+		this.cantidadProducto=1;
+	}
+	
+	public void setCantidadProducto(int cantidadProducto) {
+		if(cantidadProducto<=0) { 
+			System.out.println("LA CANTIDAD DEL PRODUCTO: "+ this.producto.getNombre() + ", NO PUEDE SER NEGATIVA O NULA!");
+		}else {
+			this.cantidadProducto=cantidadProducto; 
+		}
+	}
+	
+	public float precio() {
+		return (this.producto.getPrecioUnitario()*this.cantidadProducto);
+	}
+
+}
