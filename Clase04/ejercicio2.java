41964564b93a0236a4ebfc319523c11b0d1f1d46
Sebastianlp98 <126526039+Sebastianlp98@users.noreply.github.com>
2023-04-10 20:41:31 -0300

    Create ejercicio2.java

---
+++ Clase04/ejercicio2.java
@@ -0,0 +1,34 @@
+import java.io.IOException; //Eclipse importa automáticamente al utilizar una clase
+import java.nio.file.*; //importo el paquete para trabajar con archivos
+
+public class ej02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		args=new String[1]; //determinamos la cantidad de parámetros a aceptar
+		//en este caso es sólo la ruta
+		//envio la ruta por parámetro del "main"
+		//recordemos que el parámetro de "main" es "args"
+		args[0]="C:\\eclipse\\workspace\\ARGPROG04\\src\\archivo.txt"; //la ruta depende de dónde tengan el archivo
+		Path ruta=Paths.get(args[0]); //guardo la ruta en un objeto "Path"
+		int acumulador=0;
+		try {
+			for(String linea:Files.readAllLines(ruta)) { //recorro cada línea del archivo especificado en la ruta
+				int numero=Integer.parseInt(linea); //convertimos la línea en número
+				acumulador+=numero; //sumamos el número al total
+				//tengamos en cuenta que si escribimos mal el archivo nos dará error
+				//es decir, los números deben estar uno por cada línea
+				//además de obviamente no poner caracteres que no correspondan a los enteros
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			//este "try-catch" es obligatorio ya que estamos tratando con archivos
+			//y estos pueden cambiar su ruta o dejar de existir, por lo que Java nos obliga a tener en cuenta errores
+		}
+		System.out.println("La suma total es: "+acumulador);
+		
+	}
+
+}
