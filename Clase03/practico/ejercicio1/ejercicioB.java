2cd044c25ff26b8916adcf45ce99d8e65fe0161c
Sebastianlp98 <126526039+Sebastianlp98@users.noreply.github.com>
2023-04-10 20:52:08 -0300

    Create ejercicioB.java

---
+++ Clase03/practico/ejercicio1/ejercicioB.java
@@ -0,0 +1,17 @@
+package curso.Clase3.practico.Ejercicio1;
+
+public class EjercicioB {
+
+	public static void main(String[] args) {
+		//Utilizando variables para los numeros
+		int vectorDesordenado[] = {34,77,23};
+		int vectorOrdenado[] = new int[3];
+		boolean ordenAscendente=true;
+		
+		for(int i=0;i<3;i++) {
+			//for()int j=0;i
+		}
+				
+	}
+
+}
